6c5b25404af0ce42f83c64fa39a3357036c180bd
Pasut Ounthaisong <94848062+ExNEoxX@users.noreply.github.com>
2024-03-05 19:15:51 +0700

    Nab bar editing.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var database = require('../database');
-
+var database = require("../database");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Home'});
+router.get("/", (req, res, next) => {
+  const queryFlavors = "select distinct * from flavors order by flavor_id";
+  const queryPackages = "select distinct * from packages order by package_id";
+  const queryToppings = "select distinct * from toppings order by topping_id";
+
+  database.query(queryFlavors, (err, fetchFlavors) => {
+    if (err) {
+      console.log("Failed to query" + err);
+      return;
+    }
+
+    database.query(queryPackages, (err, fetchPackages) => {
+      if (err) {
+        console.log("Failed to query" + err);
+        return;
+      }
+
+      database.query(queryToppings, (err, fetchToppings) => {
+        if (err) {
+          console.log("Failed to query" + err);
+          return;
+        }
+
+        res.render("index", {
+          title: "English",
+          flavor_data: fetchFlavors,
+          package_data: fetchPackages,
+          topping_data: fetchToppings,
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
